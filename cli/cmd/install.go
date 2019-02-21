35bd360c3cccf981d64db4288d1482363adc0531
Alejandro Pedraza <alejandro.pedraza@gmail.com>
2019-02-21 10:30:29 -0500

    CLI inject/uninject extraction of workload parser and injection methods
    - Extracted from `/cli/cmd/inject.go` and `/cli/cmd/inject_util.go`
    the core methods doing the workload parsing and injection, and moved them into
    `/pkg/inject/inject.go`. The CLI files should now deal only with
    strictly CLI concerns, and applying the json patch returned by the new
    lib.
    - Proceeded analogously with `/cli/cmd/uninject.go` and
    `/pkg/inject/uninject.go`.
    - The `InjectReport` struct and helping methods were moved into
    `/pkg/inject/report.go`
    
    The only network call `/pkg/inject/inject.go` does is inside `shouldInject()` where it
    retrieves the namespace annotations from kubernetes. I think we need to
    review how that client is created in there.
    
    Caveats:
    
    - Currently passing aroung GlobalConfig and ProxyConfig and using their
    GRPC methods directly, until we come up with an abstraction for all that.
    - I removed support for parsing yamls with List containing multiple workloads
    because it was not an easy refactor given the recursion. I'll re-add
    that later on.
    - Todo: fix all the tests.
    - I've made a note to remove later on the bad v1 import alias that
    VSCode is adding.
    
    Ref #1748 and #2289
    
    Signed-off-by: Alejandro Pedraza <alejandro.pedraza@gmail.com>

--- cli/cmd/install.go
+++ cli/cmd/install.go
@@ -9,6 +9,8 @@ import (
 	"path"
 	"strings"
 
+	"github.com/linkerd/linkerd2/pkg/inject"
+
 	"github.com/linkerd/linkerd2/cli/static"
 	"github.com/linkerd/linkerd2/pkg/k8s"
 	uuid "github.com/satori/go.uuid"
@@ -182,7 +184,7 @@ func validateAndBuildConfig(options *installOptions) (*installConfig, error) {
 		Namespace:                   controlPlaneNamespace,
 		ControllerImage:             fmt.Sprintf("%s/controller:%s", options.dockerRegistry, options.linkerdVersion),
 		WebImage:                    fmt.Sprintf("%s/web:%s", options.dockerRegistry, options.linkerdVersion),
-		PrometheusImage:             "prom/prometheus:v2.7.1",
+		PrometheusImage:             inject.PrometheusImage,
 		PrometheusVolumeName:        "data",
 		GrafanaImage:                fmt.Sprintf("%s/grafana:%s", options.dockerRegistry, options.linkerdVersion),
 		GrafanaVolumeName:           "data",
@@ -293,10 +295,10 @@ func render(config installConfig, w io.Writer, options *installOptions) error {
 	injectOptions := newInjectOptions()
 	injectOptions.proxyConfigOptions = options.proxyConfigOptions
 
-	// Special case for linkerd-proxy running in the Prometheus pod.
-	injectOptions.proxyOutboundCapacity[config.PrometheusImage] = prometheusProxyOutboundCapacity
+	// TODO: Fetch GlobalConfig and ProxyConfig from the ConfigMap/API
+	globalConfig, proxyConfig := injectOptionsToConfigs(injectOptions)
 
-	return InjectYAML(&buf, w, ioutil.Discard, injectOptions)
+	return InjectYAML(&buf, w, ioutil.Discard, globalConfig, proxyConfig)
 }
 
 func (options *installOptions) validate() error {
